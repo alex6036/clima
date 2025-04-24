c3566303d54c2a5a62945fcea93b297ebb4d3922
alex6036 <145665271+alex6036@users.noreply.github.com>
2025-04-24 15:10:14 +0000

    estruccura

---
+++ weather_station_system/src/models/node.py
@@ -0,0 +1,5 @@
+# Node class for linked lists
+class Node:
+    def __init__(self, info):
+        self.info = info  # Data stored in the node
+        self.next = None  # Reference to the next node
